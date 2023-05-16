cdf260b57cb0ef2935b502f42943ae6825d672bf
Austin Lewis <austinlewis165@gmail.com>
2023-05-16 14:51:05 -0400

    new version with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-
-
 import Homepage from './components/home/Homepage';
+import UserPage from "./pages/UserPage";
+
+
 
 function App() {
   return (
     <>
-      <Router>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route path="/profile" element={<UserPage />} />
           {/* <Route path="/dashboard" element={<UserPage />} />
           <Route path="/logout" element={<UserPage />} /> */}
         </Routes>
-      </Router>
+  
     </>
   );
 }
