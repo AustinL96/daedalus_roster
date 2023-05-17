a3482272726cb6d5be007a4631563ba08fc7bd5d
Austin Lewis <austinlewis165@gmail.com>
2023-05-17 12:48:24 -0400

    authentication code

---
+++ client/src/utils/Users.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function Users() {
+    const [ users, setUsers ] = useState();
+    return (
+        <article>
+            <h2>Users List</h2>
+            {users?.length
+                ? (
+                    <ul>
+                        {users.map((user, i) => <li key={i}>{user?.username}</li>)}
+                    </ul>
+                ) : <p>No users to display</p>
+            }
+        </article>
+    )
+}
+
+export default Users
