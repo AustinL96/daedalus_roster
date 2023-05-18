a406fc1aff48ac2560c9a912d3655e60d71c2e29
Ryan <ryanmfloersch@gmail.com>
2023-05-17 21:35:23 -0400

    Added the create a job listing graphql elements to austins code

--- client/src/pages/CreateListing.js
+++ client/src/pages/CreateListing.js
@@ -74,15 +74,15 @@ function CreateListing({ setListing }) {
   const handleInputChange = (e) => {
     console.log(e.target.value)
     let val;
-    if(e.target.name === "salary"){
+    if (e.target.name === "salary") {
       console.log('reached?')
-       val = parseInt(e.target.value);
-    }else{
-       val = e.target.value;
+      val = parseInt(e.target.value);
+    } else {
+      val = e.target.value;
     }
     setFormData({
-      ...formData, 
-       [e.target.name]: val
+      ...formData,
+      [e.target.name]: val
     })
   }
 
@@ -116,7 +116,7 @@ function CreateListing({ setListing }) {
           as="form"
           method="POST"
           p="1rem"
-          w={{ base: "90%", md: "60%", lg: "50%"}}
+          w={{ base: "90%", md: "60%", lg: "50%" }}
           bg="gray.900"
           color={theme.colors[100]}
           border={`1px solid ${theme.colors[200]}`}
@@ -128,27 +128,29 @@ function CreateListing({ setListing }) {
           <Stack spacing="20px" p="1rem">
             <FormControl isRequired>
               <FormLabel>Job Title:</FormLabel>
-              <Input type="text" name="jobName" placeholder="Enter the job's title..." />
+              <Input value={formData.jobName} onChange={handleInputChange} type="text" name="jobName" placeholder="Enter the job's title..." />
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel>Company:</FormLabel>
-              <Input type="text" name="companyName" placeholder="Enter the hiring company..." />
+              <Input value={formData.companyName} onChange={handleInputChange} type="text" name="companyName" placeholder="Enter the hiring company..." />
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel>Location:</FormLabel>
-              <Input type="text" name="location" placeholder="Enter the address..." />
+              <Input value={formData.location} onChange={handleInputChange} type="text" name="location" placeholder="Enter the address..." />
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel>Salary:</FormLabel>
-              <Input type="text" name="salary" placeholder="50000" />
+              <Input value={formData.salary} onChange={handleInputChange} type="text" name="salary" placeholder="50000" />
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel>Job Details:</FormLabel>
               <Input
+                value={formData.jobDetails}
+                onChange={handleInputChange}
                 type="text"
                 name="jobDetails"
                 placeholder="A short description to grab attention..."
@@ -158,13 +160,15 @@ function CreateListing({ setListing }) {
             <FormControl isRequired>
               <FormLabel>Job Description:</FormLabel>
               <Textarea
+                value={formData.jobDescription} 
+                onChange={handleInputChange}
                 type="text"
                 name="jobDescription"
                 placeholder="A longer description with all information about the job..."
               />
             </FormControl>
 
-            <Button type="submit" bg={theme.colors[100]} color="gray.900" w="100%">
+            <Button onClick={handleSubmit} type="submit" bg={theme.colors[100]} color="gray.900" w="100%">
               Submit
             </Button>
           </Stack>
